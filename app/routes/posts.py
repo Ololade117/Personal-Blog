8d17fac4710da1e4fd28daa0196e8b2ffe804767
Ololade117 <170622922+Ololade117@users.noreply.github.com>
2026-07-15 23:42:37 +0000

    Added some pages

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,20 +1,30 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 
 from app.database.dependencies import get_db
 from app.models.post import Post
+from app.templates import templates
 
 router = APIRouter()
 
-@router.get("/")
-def home(db: Session = Depends(get_db)):
+@router.get("/", response_class=HTMLResponse)
+def home(request: Request,
+         db: Session = Depends(get_db)):
     posts = db.query(Post).all()
-    return posts
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={
+            "request": request,
+            "posts": posts,
+        },
+    )
 
-@router.post("/admin/new")
+@router.post("/admin/new", response_class=HTMLResponse)
 def create_post(
-    title: str,
-    content: str,
+    title: str = Form(...),
+    content: str = Form(...),
     db: Session = Depends(get_db)
 ):
     post = Post(
@@ -24,11 +34,15 @@ def create_post(
     db.add(post)
     db.commit()
     db.refresh(post)
-    return post
+    return RedirectResponse(
+        url="/",
+        status_code=303
+    )
 
-@router.get("/posts/{post_id}")
+@router.get("/posts/{post_id}", response_class=HTMLResponse)
 def get_post(
     post_id: int,
+    request: Request,
     db: Session = Depends(get_db)
 ):
     post = (
@@ -37,4 +51,26 @@ def get_post(
         .first()
     )
 
-    return post
+    return templates.TemplateResponse(
+        request=request,
+        name="post.html",
+        context={
+            "request": request,
+            "post": post,
+        },
+    )
+
+@router.get(
+    "/admin/new",
+    response_class=HTMLResponse
+)
+def new_post_page(
+    request: Request
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="create.html",
+        context={
+            "request": request,
+        },
+    )
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI 
 from app.routes import posts
 from fastapi.staticfiles import StaticFiles
-
+from app.templates import templates
 
 app = FastAPI()
 
@@ -12,6 +12,9 @@ app.mount(
 )
 
 app.include_router(posts.router)
-@app.get("/")
-def home():
-    return {"message:" "Personal Blog API is running"}
+
+@app.get("/health")
+def health():
+    return {"message": "Personal Blog API is running"}
+
+
